85ddeeb3e044b9f09d6e394994f8b6eac24027b3
SPARATA <ahmedsoudani09@gmail.com>
2023-07-19 14:31:32 +0100

    start working

--- firstdjango/hello/views.py
+++ firstdjango/hello/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return render(request, "hello/index.html")
+
+def mor(request):
+    return HttpResponse("Ahla, bmortadha lebnin!")
+
+def greet(request, name):
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+    })
