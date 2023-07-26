2908b14944f6f0cc8b11307bd90905031f416bae
SPARATA <ahmedsoudani09@gmail.com>
2023-07-26 22:41:05 +0100

    add search and index

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("search_result", views.search_result, name="search_result"),
     path("<str:title>", views.wiki_page_view, name="wiki_page"),
+    
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -11,19 +11,31 @@ def index(request):
     })
 
 def wiki_page_view(request, title):
-    title = title + '.md'
+    file_path = os.path.abspath(os.path.join("entries", title + '.md'))
+
     try:
-        html = markdown2.markdown(title)
+        with open(file_path, 'r') as file:
+            markdown_content = file.read()
+        html = markdown2.markdown(markdown_content)
     except FileNotFoundError:
-        return render(request, "encyclopedia/Page_error")
-    title = title.replace('.md', '')
-    return render(request, "encyclopedia/wiki_page.html", {
-            'title': title,
-            'content' : html
+        return render(request, "encyclopedia/Page_error.html", {
+            "title": title,
         })
-def check(title):
+
+    title = title.replace('.md', '')
+
+    return render(request, "encyclopedia/wiki_page.html", {
+        'title': title,
+        'content': html
+    })
+
+def search_result(request):
+    query = request.GET.get('q', '').strip()
     entries = util.list_entries()
-    if title in entries:
-        return False
-    else:
-        return True 
+
+    results = [entry for entry in entries if query.lower() in entry.lower()]
+
+    return render(request, "encyclopedia/search_result.html", {
+        "results" : results,
+        "query": query
+    })
